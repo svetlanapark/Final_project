ac32d59c692a796001fb7fe0c34ebfb34c3d5d19
svetlanapark <89245690+svetlanapark@users.noreply.github.com>
2021-09-21 22:16:10 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let popupBg = document.querySelector('.popup_bg');
+let popup = document.querySelector('.popup');
+let openPopupButtons = document.querySelectorAll('.open-popup');
+let closePopupButton = document.querySelector('.close-popup');
+
+openPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+    })
+});
+
+closePopupButton.addEventListener('click', () => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+document.addEventListener('click', (e) => {
+    if (e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
+
+
+
+function text() {
+    let btn = document.querySelector("#submit");
+    if (input.length < 1) {
+        btn.disabled = true;
+    } else {
+        btn.disabled = false;
+        document.querySelector("#submit").onclick = function () {
+        }
+    }
+}
